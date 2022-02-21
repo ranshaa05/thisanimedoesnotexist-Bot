bbbe14802703e96a11ba6b3d5de74306e5657c85
ranshaa05 <ranshaa05@gmail.com>
2022-02-21 17:50:57 +0200

    major re-factoring of the code.

--- Client.py
+++ Client.py
@@ -14,7 +14,7 @@ connected_users = []
 @client.event
 async def on_ready():
     await client.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.listening, name="$waifu"))
-    print("Bot is ready!")
+    print("\033[1;32;40mBot is ready!\033[0;37;40m")
 
 @client.event
 async def on_command_error(ctx, error):
@@ -24,7 +24,6 @@ async def on_command_error(ctx, error):
 
 @client.command()
 async def waifu(ctx):
-
     if ctx.message.content.lower() == "$waifu start":
         return
 
@@ -60,105 +59,104 @@ async def waifu(ctx):
     print("\033[1;37;40mEvent: \033[93mBot closed for user '" + str(ctx.author.name) + "'.\033[0;37;40m")
     
 
-
 async def ask_for_seed(ctx):
-    global msg, seed
+    global seed
     await ctx.channel.send("The seed determines the base picture. You can either type a number or type 'random' to get a random seed.\nWhat Seed?")
     await list_last_msg_id(ctx, msg_user_binder, client)
-    try:
-        msg = await client.wait_for("message", timeout=120)
-        while not await check(msg, ctx):
-            msg = await client.wait_for("message", timeout=120)
-    except TimeoutError:
-        return False
-
-    seed = msg.content
+    seed = await get_message(ctx)
     
-    if seed.lower() == "random":
-        seed = str(randint(1, 10000))
-        await ctx.channel.send("Seed is " + seed)
-        await list_last_msg_id(ctx, msg_user_binder, client)
-        sleep(2)
-    
-    if seed.lower() == "exit":
-        return False
-
-    if not seed.isnumeric():
-        await ctx.channel.send("Seeds have to be numbers only! You can type 'exit' to exit or try again.")
-        await list_last_msg_id(ctx, msg_user_binder, client)
-        return await ask_for_seed(ctx)
-
-    if len(seed) <= 5:
-        for i in range(4):
-            if len(seed) == 5:
-                await delete_messages(ctx, msg_user_binder, client)
-                return
-            seed = "0" + seed
-    
-
-    else:
+    while not seed == "random" and not seed == "exit" and not int(seed) in range(0, 1000000):
         await ctx.channel.send("Seeds can only be between 0 and 99999. Try again.")
         await list_last_msg_id(ctx, msg_user_binder, client)
-        return await ask_for_seed(ctx)
-    
+        seed = await get_message(ctx)
+        
+
+    seed = await random(ctx, 99999, seed) #TODO: this seems to always generate a 5 digit number for some reason.
+
+    if await stop(seed):
+        return False
+
+    while len(seed) < 5:
+        seed = "0" + seed
+
     await delete_messages(ctx, msg_user_binder, client)
     await ctx.channel.send("Great! Now let's move on to the next part.")
     await list_last_msg_id(ctx, msg_user_binder, client)
+    return seed
     
 
 
 async def ask_for_creativity_level(ctx):
-    global msg
     await ctx.channel.send("This part will determine how creative the AI will be when creating the picture. 'random' is also an option here :wink:\nAI creativity level? (1-18)")
     await list_last_msg_id(ctx, msg_user_binder, client)
-    try:
-        msg = await client.wait_for("message", timeout=120)
-        while not await check(msg, ctx):
-            msg = await client.wait_for("message", timeout=120)
-    except TimeoutError:
+    creativity_value = await get_message(ctx)
+    creativity_value = await random(ctx, 18, creativity_value)
+
+    if await stop(creativity_value):
         return False
 
-    creativity_value = msg.content
-
-    if creativity_value.lower() == "random":
-        creativity_value = str(randint(1, 18))
-        await ctx.channel.send("Creativity level is " + creativity_value)
-        await list_last_msg_id(ctx, msg_user_binder, client)
-        sleep(2)
-
-    if creativity_value.lower() == "exit":
-        return False
-
-    if not creativity_value.isnumeric():
-        await ctx.channel.send("Creativity Values have to be numbers only! You can type 'exit' to exit or try again.")
-        await list_last_msg_id(ctx, msg_user_binder, client)
-        return await ask_for_creativity_level(ctx)
-    
     creativity_value = int(creativity_value)
-    if creativity_value > 18 or creativity_value < 1:
+    while not creativity_value in range(1, 19):
         await ctx.channel.send("Creativity Values have to be between 1 and 18. Try again")
         await list_last_msg_id(ctx, msg_user_binder, client)
-        return await ask_for_creativity_level(ctx)
+        creativity_value = int(await get_message(ctx))
+
+
+    global psi_level
+    psi_level = 0.2
+    for i in range(creativity_value):
+        psi_level = psi_level + 0.1
+    await delete_messages(ctx, msg_user_binder, client)
+    return psi_level
+        
+    
+
+
+async def random(ctx, max_range, value):
+    if str(value).lower() == "random":
+        value = str(randint(1, max_range))
+        await ctx.channel.send(f"Selected {value}")
+        await list_last_msg_id(ctx, msg_user_binder, client)
+        sleep(2)
+        return value
     else:
-        global psi_level
-        psi_level = 0.2
-        for i in range(creativity_value):
-            psi_level = psi_level + 0.1
-        await delete_messages(ctx, msg_user_binder, client)
-        
-        
+        return value
+
+async def stop(value):
+    if value.lower() == "exit":
+        return True
+    else:
+        return False
+
+
 
 
 
 async def check(msg, ctx):
     if not (msg.author == ctx.author and msg.channel == ctx.channel):
         return False
-    if msg.content == "$waifu":
+    
+    msg = msg.content
+    if msg == "$waifu":
+        return False
+
+    if not msg.isnumeric() and msg != "random" and msg != "exit":                   #makes sure that the user only types numbers or 'random' or 'exit'
+        await ctx.channel.send("Values have to be numbers only! You can type 'exit' to exit or try again.")
+        await list_last_msg_id(ctx, msg_user_binder, client)
         return False
     else:
         return True
-        
-    
-    
+
+async def get_message(ctx):
+    global msg
+    try:
+        msg = await client.wait_for("message", timeout=120)
+        while not await check(msg, ctx):
+            msg = await client.wait_for("message", timeout=120)
+        return msg.content
+    except TimeoutError:
+        return False
+
+
 
 client.run(secret)
