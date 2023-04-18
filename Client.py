a7ab6ecf391d5bdd0740d6dae192b8b9fed07a34
ranshaa05 <ranshaa05@gmail.com>
2023-04-18 11:19:10 +0300

    black- formatting

--- Client.py
+++ Client.py
@@ -8,17 +8,22 @@ from nextcord.ext import commands
 from typing import Optional
 
 
-#logging config
+# logging config
 coloredlogs.install()
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+)
 logging.getLogger("nextcord").setLevel(logging.WARNING)
 
-#bot config
-client = commands.Bot(intents = nextcord.Intents().all(), case_insensitive=True)
+# bot config
+client = commands.Bot(intents=nextcord.Intents().all(), case_insensitive=True)
+
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.listening, name="/anime"))
+    await client.change_presence(
+        activity=nextcord.Activity(type=nextcord.ActivityType.listening, name="/anime")
+    )
     logging.info("Bot is ready!")
 
 
@@ -31,14 +36,26 @@ with open(TOKEN_FILE, "r") as file:
 
 
 @client.slash_command()
-async def anime(interaction: nextcord.Interaction,
-                seed: nextcord.Range[1, 100000] = nextcord.SlashOption(description="Seed to base the picture off of.", required=True, default=1),
-                creativity: nextcord.Range[1, 18] = nextcord.SlashOption(description="Creativity level for the picture.", required=True, default=1),
-                random_seed: Optional[bool] = nextcord.SlashOption(description="Randomizes the seed (seed value doesn't matter).", required=False),
-                random_creativity: Optional[bool] = nextcord.SlashOption(description="Randomizes the creativity level (creativity value doen't matter).", required=False)):
+async def anime(
+    interaction: nextcord.Interaction,
+    seed: nextcord.Range[1, 100000] = nextcord.SlashOption(
+        description="Seed to base the picture off of.", required=True, default=1
+    ),
+    creativity: nextcord.Range[1, 18] = nextcord.SlashOption(
+        description="Creativity level for the picture.", required=True, default=1
+    ),
+    random_seed: Optional[bool] = nextcord.SlashOption(
+        description="Randomizes the seed (seed value doesn't matter).", required=False
+    ),
+    random_creativity: Optional[bool] = nextcord.SlashOption(
+        description="Randomizes the creativity level (creativity value doen't matter).",
+        required=False,
+    ),
+):
     "Fetches a picture from an anime that does not exist yet from https://thisanimedoesnotexist.ai."
 
-    #this is in order to include 0 as a valid seed, as nextxord allows negative numbers if min is 0
+    logging.info(f"Bot started by user {interaction.user}")
+    # this is in order to include 0 as a valid seed, as nextxord allows negative numbers if min is 0
     seed = seed - 1
     creativity = creativity - 1
 
@@ -50,20 +67,23 @@ async def anime(interaction: nextcord.Interaction,
         creativity = random.choice(range(0, 18))
     psi_value = await make_creativity_valid(creativity)
 
-    await interaction.response.send_message(f"""Here's your Anime! I hope you like it! :slight_smile:
+    await interaction.response.send_message(
+        f"""Here's your Anime! I hope you like it! :slight_smile:
 *_Seed: {seed}_* || *_Creativity Level: {psi_value}_*
-https://thisanimedoesnotexist.ai/results/psi-{psi_value}/seed{seed}.png""")
-
+https://thisanimedoesnotexist.ai/results/psi-{psi_value}/seed{seed}.png"""
+    )
+    logging.info(f"Bot finished for user {interaction.user}")
 
 
 async def make_seed_valid(seed):
     seed = f"{seed:05}"
     return seed
-    
+
+
 async def make_creativity_valid(creativity_value):
     creativity_value = round(0.1 * creativity_value + 0.3, 1)
     return creativity_value
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     client.run(TOKEN)
